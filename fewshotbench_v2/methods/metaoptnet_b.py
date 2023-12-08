d9349bc0aa8efc80272251d74651a940c6aef4d4
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-08 14:46:12 +0100

    method.helpers

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -46,6 +46,8 @@ class MetaOptNet(MetaTemplate):
         # scores = self.classifier(z_query)
         # scores = -euclidean_dist(scores_query, scores_support)
         print(x.shape)
+        print(z_support.shape)
+        print(z_query.shape)
 
         tasks_per_batch = z_query.size(0)
         n_support = z_support.size(1)
