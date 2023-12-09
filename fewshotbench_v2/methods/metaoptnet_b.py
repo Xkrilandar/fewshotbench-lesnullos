fa17e86f0f45a4b007671026a6d408e0adb02a1e
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-09 17:36:48 +0100

    delete prints

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -77,10 +77,8 @@ class MetaOptNet(MetaTemplate):
         block_kernel_matrix += 1.0 * torch.eye(self.n_way*n_support).expand(tasks_per_batch, self.n_way*n_support, self.n_way*n_support).cuda()
         
         original_labels = y_support.reshape(tasks_per_batch * n_support) # ??? OU PAS)
-        print(original_labels)
         label_mapping = {label: i for i, label in enumerate(set(torch.unique(original_labels).tolist()))}
         support_labels = torch.tensor([label_mapping[label.item()] for label in original_labels]).to('cuda')
-        print(support_labels)
         support_labels_one_hot = one_hot(support_labels, self.n_way) # (tasks_per_batch * n_support, n_support)
         support_labels_one_hot = support_labels_one_hot.view(tasks_per_batch, n_support, self.n_way)
         support_labels_one_hot = support_labels_one_hot.reshape(tasks_per_batch, n_support * self.n_way)
@@ -138,8 +136,6 @@ class MetaOptNet(MetaTemplate):
 
         scores = self.set_forward(x, y)
         scores = scores.view(self.n_query * self.n_way, -1)
-        print("y_query", y_query.size())
-        print("scores", scores.size())
         return self.loss_fn(scores, y_query)
     
     def train_loop(self, epoch, train_loader, optimizer):  # overwrite parrent function
@@ -279,7 +275,6 @@ def one_hot(indices, depth):
 
     encoded_indicies = torch.zeros(indices.size() + torch.Size([depth])).cuda()
     index = indices.view(indices.size()+torch.Size([1]))
-    print(index)
     encoded_indicies = encoded_indicies.scatter_(1,index,1)
     
     return encoded_indicies
