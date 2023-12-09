bb4fa64e79427b7b1082b4ebfa7105176ba4211c
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-09 16:13:18 +0100

    pip qpth

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -5,6 +5,7 @@ from torch.autograd import Variable
 from methods.meta_template import MetaTemplate
 import cvxpy as cp
 import wandb
+from qpth.qp import QPFunction
 
 # class DifferentiableSVM(nn.Module):
 #     def __init__(self, num_features, num_classes):
@@ -110,8 +111,9 @@ class MetaOptNet(MetaTemplate):
         #        \hat z =   argmin_z 1/2 z^T G z + e^T z
         #                 subject to Cz <= h
         # We use detach() to prevent backpropagation to fixed variables.
-        #qp_sol = QPFunction(verbose=False, maxIter=maxIter)(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach())
-        qp_sol = solve_qp(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach(), n_support)
+        maxIter = 15
+        qp_sol = QPFunction(verbose=False, maxIter=maxIter)(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach())
+        #qp_sol = solve_qp(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach(), n_support)
 
         # Compute the classification score.
         compatibility = computeGramMatrix(z_support, z_query)
