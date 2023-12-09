4b3abc7e57aefa9cfe24b8eaa2c1989080062bb4
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-09 15:45:35 +0100

    relabelling

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -79,7 +79,7 @@ class MetaOptNet(MetaTemplate):
         print(original_labels)
         label_mapping = {label: i for i, label in enumerate(set(torch.unique(original_labels).tolist()))}
         support_labels = torch.tensor([label_mapping[label.item()] for label in original_labels])
-        print(support_labels)
+        print(support_labels).to('cuda')
         support_labels_one_hot = one_hot(support_labels, self.n_way) # (tasks_per_batch * n_support, n_support)
         support_labels_one_hot = support_labels_one_hot.view(tasks_per_batch, n_support, self.n_way)
         support_labels_one_hot = support_labels_one_hot.reshape(tasks_per_batch, n_support * self.n_way)
