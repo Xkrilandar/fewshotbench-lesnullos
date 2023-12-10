7e93ca09462e21cea0c52226dcee5ed0b0d366b8
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-10 19:23:31 +0100

    test maml

--- fewshotbench_v2/methods/maml.py
+++ fewshotbench_v2/methods/maml.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import wandb
 from torch.autograd import Variable
+import sys
 
 from backbones.blocks import Linear_fw
 from methods.meta_template import MetaTemplate
@@ -106,6 +107,8 @@ class MAML(MetaTemplate):
         if torch.cuda.is_available():
             y_b_i = y_b_i.cuda()
 
+        print("scores", scores)
+        sys.exit()
         loss = self.loss_fn(scores, y_b_i)
 
         return loss
@@ -133,6 +136,7 @@ class MAML(MetaTemplate):
                 y = None
 
             loss = self.set_forward_loss(x, y)
+
             avg_loss = avg_loss + loss.item()
             loss_all.append(loss)
 
--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -8,28 +8,6 @@ import wandb
 from qpth.qp import QPFunction
 import torch.nn.functional as F
 
-# class DifferentiableSVM(nn.Module):
-#     def __init__(self, num_features, num_classes):
-#         super(DifferentiableSVM, self).__init__()
-#         # Initialize weights and biases for the SVM
-#         self.weights = nn.Parameter(torch.randn(num_classes, num_features))
-#         self.bias = nn.Parameter(torch.randn(num_classes))
-
-#     def forward(self, x):
-#         # Linear decision function: Wx + b
-#         return torch.matmul(x, self.weights.t()) + self.bias
-
-#     def hinge_loss(self, outputs, labels):
-#         # Implement hinge loss function for SVM
-#         # Note: labels should be +1 or -1
-#         hinge_loss = torch.mean(torch.clamp(1 - outputs.t() * labels, min=0))
-#         return hinge_loss
-
-#     def regularization_loss(self):
-#         # L2 regularization loss (optional)
-#         reg_loss = torch.norm(self.weights, p=2)
-#         return reg_loss
-
 class MetaOptNet(MetaTemplate):
     def __init__(self, backbone, n_way, n_support, num_classes, num_features):
         super(MetaOptNet, self).__init__(backbone, n_way, n_support)
@@ -41,14 +19,6 @@ class MetaOptNet(MetaTemplate):
     def set_forward(self, x, y, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature)
         y_support, y_query = self.parse_feature(y, True)
-
-        # z_support = z_support.contiguous()
-        # z_proto = z_support.view(self.n_way, self.n_support, -1).mean(1)  # the shape of z is [n_data, n_dim]
-        # z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
-
-        # scores_support = self.classifier(z_proto)
-        # scores = self.classifier(z_query)
-        # scores = -euclidean_dist(scores_query, scores_support)
 
         tasks_per_batch = z_query.size(0)
         n_support = z_support.size(1)
