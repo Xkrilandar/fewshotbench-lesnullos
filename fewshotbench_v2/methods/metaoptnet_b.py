87aae433c3c9ad68a7ee15f5fa44deff03f0cfe3
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-11 18:27:54 +0100

    changing things a little bit

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -248,8 +248,8 @@ class MetaOptNet(MetaTemplate):
     def correct(self, x, y):
         _, y_query = self.parse_feature(y, is_feature=True)
         scores = self.set_forward(x)
-        #y_query = np.repeat(range(self.n_way), self.n_query)
-
+        #y_query = np.repeat(range(self.n_way), self.n_query))
+        y_query = y_query.reshape(-1)
         _, topk_labels = scores.data.topk(1, 1, True, True)
         topk_ind = topk_labels.cpu().numpy()
         top1_correct = np.sum(topk_ind[:, 0] == y_query)
