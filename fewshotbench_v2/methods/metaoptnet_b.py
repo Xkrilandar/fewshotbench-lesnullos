5fcc4b4f752ee0b21890f0821ad0eda7d68dfa7e
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-11 13:47:14 +0100

    changing things a little bit

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -40,6 +40,8 @@ class MetaOptNet(MetaTemplate):
     def set_forward(self, x, y, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature)
         y_support, y_query = self.parse_feature(y, True)
+        print("y_supoooooooooooooooooooooort",y_support)
+        print("queeeeeeeeeeeery",y_query)
         # z_support = z_support.contiguous()
         # z_proto = z_support.view(self.n_way, self.n_support, -1).mean(1)  # the shape of z is [n_data, n_dim]
         # z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
