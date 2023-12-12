b94c131169d63c37c1a669058bdc53fae3ebf199
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-12 11:41:12 +0100

    a new day

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -75,8 +75,9 @@ class MetaOptNet(MetaTemplate):
 
             logits = qp_sol.float().unsqueeze(1).expand(tasks_per_batch, n_query, n_support)
             logits = logits * compatibility
-            logits = logits.view(tasks_per_batch, n_query, self.n_shot, self.n_way)
+            logits = logits.view(tasks_per_batch, n_query, n_support, self.n_way)
             logits = torch.sum(logits, 2)
+
         if method==1:
             original_labels = y_support.reshape(tasks_per_batch * n_support) # ??? OU PAS)
             support_labels = torch.tensor(map_labels(original_labels))
