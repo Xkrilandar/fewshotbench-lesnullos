e463800dd906f8de061a4fdfd1c0ff2751171292
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-12 10:02:03 +0100

    proto

--- fewshotbench_v2/methods/protonet.py
+++ fewshotbench_v2/methods/protonet.py
@@ -20,8 +20,10 @@ class ProtoNet(MetaTemplate):
         z_support = z_support.contiguous()
         z_proto = z_support.view(self.n_way, self.n_support, -1).mean(1)  # the shape of z is [n_data, n_dim]
         z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
+        
 
         dists = euclidean_dist(z_query, z_proto)
+        print("dists", dists)
         scores = -dists
         return scores
 
