935949093856d7ecd6c4a828e73889c99c0a73bc
cs210-bbernath~ <bastien.bernath@epfl.ch>
2023-12-12 16:51:06 +0100

    a new day

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -78,7 +78,7 @@ class MetaOptNet(MetaTemplate):
             logits = logits * compatibility
             logits = logits.view(tasks_per_batch, n_query, self.n_way)
             logits = torch.sum(logits, 2)
-            logits = logits.view(-1, self.n_way)
+            logits = logits.view(-1)
             print(logits.shape)
 
         if method==1:
@@ -128,7 +128,6 @@ class MetaOptNet(MetaTemplate):
         #qp_sol = solve_qp(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach(), n_support)
         scores = self.set_forward(x,y_support, method = METHOD)
         #self.y_query = torch.tensor(y_query.reshape(-1).tolist()).to('cuda')
-        print("scooore", scores.shape)
         y_query = y_query.reshape(-1)
         y_query = torch.tensor(map_labels(y_query)).to('cuda')
         ret = self.loss_fn(scores, y_query)
