723580f3df365046f415b14273825d30075df311
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-12 18:06:25 +0100

    ?

--- fewshotbench_v2/methods/metaoptnet_b.py
+++ fewshotbench_v2/methods/metaoptnet_b.py
@@ -8,68 +8,20 @@ from qpth.qp import QPFunction
 import cvxpy as cp
 import wandb
 
-# class DifferentiableSVM(nn.Module):
-#     def __init__(self, num_features, num_classes):
-#         super(DifferentiableSVM, self).__init__()
-#         # Initialize weights and biases for the SVM
-#         self.weights = nn.Parameter(torch.randn(num_classes, num_features))
-#         self.bias = nn.Parameter(torch.randn(num_classes))
-
-#     def forward(self, x):
-#         # Linear decision function: Wx + b
-#         return torch.matmul(x, self.weights.t()) + self.bias
-
-#     def hinge_loss(self, outputs, labels):
-#         # Implement hinge loss function for SVM
-#         # Note: labels should be +1 or -1
-#         hinge_loss = torch.mean(torch.clamp(1 - outputs.t() * labels, min=0))
-#         return hinge_loss
-
-#     def regularization_loss(self):
-#         # L2 regularization loss (optional)
-#         reg_loss = torch.norm(self.weights, p=2)
-#         return reg_loss
 
 class MetaOptNet(MetaTemplate):
-    def __init__(self, backbone, n_way, n_support, num_classes, num_features):
+    def __init__(self, backbone, n_way, n_support):
         super(MetaOptNet, self).__init__(backbone, n_way, n_support)
-        #self.classifier = DifferentiableSVM(num_classes=num_classes, num_features=num_features) 
         self.loss_fn = nn.CrossEntropyLoss()
         self.C_reg = 0.1
         print(self.n_way)
 
-
     def set_forward(self, x, y_support, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature=False)
-        # z_support = z_support.contiguous()
-        # z_proto = z_support.view(self.n_way, self.n_support, -1).mean(1)  # the shape of z is [n_data, n_dim]
-        # z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
-
-        # scores_support = self.classifier(z_proto)
-        # scores = self.classifier(z_query)
-        # scores = -euclidean_dist(scores_query, scores_support)
 
         tasks_per_batch = z_query.size(0)
         n_support = z_support.size(1)
         n_query = z_query.size(1)
-
-        assert(z_query.dim() == 3)
-        assert(z_support.dim() == 3)
-        assert(z_query.size(0) == z_support.size(0) and z_query.size(2) == z_support.size(2))
-        #assert(n_support == n_way * n_shot)      # n_support must equal to n_way * n_shot
-
-        #Here we solve the dual problem:
-        #Note that the classes are indexed by m & samples are indexed by i.
-        #min_{\alpha}  0.5 \sum_m ||w_m(\alpha)||^2 + \sum_i \sum_m e^m_i alpha^m_i
-        #s.t.  \alpha^m_i <= C^m_i \forall m,i , \sum_m \alpha^m_i=0 \forall i
-
-        #where w_m(\alpha) = \sum_i \alpha^m_i x_i,
-        #and C^m_i = C if m  = y_i,
-        #C^m_i = 0 if m != y_i.
-        #This borrows the notation of liblinear.
-        
-        #\alpha is an (n_support, n_way) matrix
-        
 
         original_labels = y_support.reshape(tasks_per_batch * n_support) # ??? OU PAS)
         support_labels = torch.tensor(map_labels(original_labels))
@@ -77,19 +29,16 @@ class MetaOptNet(MetaTemplate):
         support_labels_one_hot = support_labels_one_hot.view(tasks_per_batch, n_support, self.n_way)
         support_labels_one_hot = support_labels_one_hot.reshape(tasks_per_batch, n_support * self.n_way)
         
-        
         qp_sol = self.qp_solve(support_labels_one_hot, z_support, n_support, tasks_per_batch)
-        compatibility = computeGramMatrix(z_support, z_query)
+        compatibility = computeGramMatrix(z_query, z_query)
         compatibility = compatibility.float()
         compatibility = compatibility.unsqueeze(3).expand(tasks_per_batch, n_support, n_query, self.n_way)
         qp_sol = qp_sol.reshape(tasks_per_batch, n_support, self.n_way)
         logits = qp_sol.float().unsqueeze(2).expand(tasks_per_batch, n_support, n_query, self.n_way)
         logits = logits * compatibility
         logits = torch.sum(logits, 1)
-        # Reshape logits to the desired shape
+
         logits = logits.view(-1, self.n_way)
-
-
         return logits
 
     def set_forward_loss(self, x, y):
@@ -184,32 +133,22 @@ class MetaOptNet(MetaTemplate):
 
         id_matrix_0 = torch.eye(self.n_way).expand(tasks_per_batch, self.n_way, self.n_way).cuda()
         block_kernel_matrix = batched_kronecker(kernel_matrix, id_matrix_0)
-        #This seems to help avoid PSD error from the QP solver.
+
         block_kernel_matrix += 1.0 * torch.eye(self.n_way*n_support).expand(tasks_per_batch, self.n_way*n_support, self.n_way*n_support).cuda()
         G = block_kernel_matrix
         e = -1.0 * support_labels_one_hot
-        #This part is for the inequality constraints:
-        #\alpha^m_i <= C^m_i \forall m,i
-        #where C^m_i = C if m  = y_i,
-        #C^m_i = 0 if m != y_i.
+        
         id_matrix_1 = torch.eye(self.n_way * n_support).expand(tasks_per_batch, self.n_way * n_support, self.n_way * n_support)
         C = Variable(id_matrix_1)
         h = Variable(self.C_reg * support_labels_one_hot)
 
-        #This part is for the equality constraints:
-        #\sum_m \alpha^m_i=0 \forall i
         id_matrix_2 = torch.eye(n_support).expand(tasks_per_batch, n_support, n_support).cuda()
 
         A = Variable(batched_kronecker(id_matrix_2, torch.ones(tasks_per_batch, 1, self.n_way).cuda()))
         b = Variable(torch.zeros(tasks_per_batch, n_support))
-        #print (A.size(), b.size())
         G, e, C, h, A, b = [x.float().cuda() for x in [G, e, C, h, A, b]]
 
-        # Solve the following QP to fit SVM:
-        #        \hat z =   argmin_z 1/2 z^T G z + e^T z
-        #                 subject to Cz <= h
-        # We use detach() to prevent backpropagation to fixed variables.
-        maxIter = 15
+        maxIter = 1
         return QPFunction(verbose=False, maxIter=maxIter)(G, e.detach(), C.detach(), h.detach(), A.detach(), b.detach())
     
 
@@ -220,39 +159,9 @@ def map_labels(labels):
     return [label_mapping[label.item()] for label in labels]
 
 def computeGramMatrix(A, B):
-    """
-    Constructs a linear kernel matrix between A and B.
-    We assume that each row in A and B represents a d-dimensional feature vector.
-    
-    Parameters:
-      A:  a (n_batch, n, d) Tensor.
-      B:  a (n_batch, m, d) Tensor.
-    Returns: a (n_batch, n, m) Tensor.
-    """
-    
-    assert(A.dim() == 3)
-    assert(B.dim() == 3)
-    assert(A.size(0) == B.size(0) and A.size(2) == B.size(2))
-
     return torch.bmm(A, B.transpose(1,2))
 
-def solve_qp(Q, c, G, h, A, b, n_support):
-    # Create a variable to optimize
-    # x = cp.Variable(len(c))
-    x = cp.Variable(n_support)
 
-    # Define the objective function
-    objective = cp.Minimize(0.5 * cp.quad_form(x, Q) + x)
-
-    # Define the constraints
-    # constraints = [G @ x <= h, A @ x == b]
-    constraints = [A @ x == b]
-
-    # Define the problem and solve it
-    prob = cp.Problem(objective, constraints)
-    prob.solve()
-
-    return x.value
 
 def batched_kronecker(matrix1, matrix2):
     matrix1_flatten = matrix1.reshape(matrix1.size()[0], -1)
@@ -260,19 +169,3 @@ def batched_kronecker(matrix1, matrix2):
     return torch.bmm(matrix1_flatten.unsqueeze(2), matrix2_flatten.unsqueeze(1)).reshape([matrix1.size()[0]] + list(matrix1.size()[1:]) + list(matrix2.size()[1:])).permute([0, 1, 3, 2, 4]).reshape(matrix1.size(0), matrix1.size(1) * matrix2.size(1), matrix1.size(2) * matrix2.size(2))
 
 
-# def one_hot(indices, depth):
-#     """
-#     Returns a one-hot tensor.
-#     This is a PyTorch equivalent of Tensorflow's tf.one_hot.
-        
-#     Parameters:
-#       indices:  a (n_batch, m) Tensor or (m) Tensor.
-#       depth: a scalar. Represents the depth of the one hot dimension.
-#     Returns: a (n_batch, m, depth) Tensor or (m, depth) Tensor.
-#     """
-
-#     encoded_indicies = torch.zeros(indices.size() + torch.Size([depth])).cuda()
-#     index = indices.view(indices.size()+torch.Size([1]))
-#     encoded_indicies = encoded_indicies.scatter_(1,index,1)
-    
-#     return encoded_indicies
