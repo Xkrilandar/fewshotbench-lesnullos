baf1622f5206d8ec44fd0845be8596e674d8caa9
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-12-12 18:42:31 +0100

    implementation of basic svm with 60% acc

---
+++ fewshotbench_v2/methods/svm.py
@@ -0,0 +1,51 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from methods.meta_template import MetaTemplate
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import sys
+
+class MetaOptNet(MetaTemplate):
+    def __init__(self, backbone, n_way, n_support):
+        super(MetaOptNet, self).__init__(backbone, n_way, n_support)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.clf = make_pipeline(StandardScaler(), SVC(kernel='linear', C=0.5))
+
+
+    def set_forward(self, x, is_feature=False):
+        z_support, z_query = self.parse_feature(x, is_feature)
+        print(z_query.size())
+        z_support = z_support.contiguous().view(self.n_way * self.n_support, -1)
+        z_query = z_query.contiguous().view(self.n_way * self.n_query, -1)
+        print(z_query.size())
+        
+        support_labels = Variable(torch.from_numpy(np.repeat(range(self.n_way), self.n_support)).cuda())
+
+        support_labels_cpu = support_labels.cpu().detach().numpy()
+        z_support_cpu = z_support.cpu().detach().numpy()
+        z_query_cpu = z_query.cpu().detach().numpy()
+
+        # print("support_labels", support_labels_cpu)
+        # print("z_support", z_support_cpu)
+        self.clf.fit(z_support_cpu, support_labels_cpu)
+        
+        scores = self.clf.decision_function(z_query_cpu)
+        # print("scores", scores)
+        scores_torch = Variable(torch.from_numpy(scores).cuda(), requires_grad=True)
+        return scores_torch
+
+
+    def set_forward_loss(self, x):
+        y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query ))
+        y_query = Variable(y_query.cuda())
+
+        
+        scores = self.set_forward(x)
+        print(scores.size())
+        sys.exit()
+        return self.loss_fn(scores, y_query )
